2c1491cb105e9a6d7b848220583148fa31eb9b2f
JulianAVG64 <julvargasgu@unal.edu.co>
2025-05-21 10:14:32 -0500

    Add livewire image support and validation with HTML

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -84,6 +84,7 @@
             class="block mt-1 w-full"
             type="file"
             wire:model="imagen"
+            accept="image/*"
         />
         <x-input-error :messages="$errors->get('imagen')" class="mt-2" />
     </div>
