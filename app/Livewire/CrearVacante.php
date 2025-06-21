6dbd23ef69f6023b6c4c7e48d708af4c199a1e85
JulianAVG64 <julvargasgu@unal.edu.co>
2025-06-21 15:01:19 -0500

    Storing vacancie with image

--- app/Livewire/CrearVacante.php
+++ app/Livewire/CrearVacante.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Categoria;
 use App\Models\Salario;
+use App\Models\Vacante;
 use Livewire\Component;
 use Livewire\Features\SupportFileUploads\WithFileUploads;
 
@@ -32,6 +33,29 @@ class CrearVacante extends Component
     public function crearVacante()
     {
         $datos = $this->validate();
+
+        // Almacenar la imagen
+        $imagen = $this->imagen->store('public/vacantes');
+        $datos['imagen'] = str_replace('public/vacantes/', '', $imagen);
+        // dd($nombre_imagen);
+
+        // Crear la Vacante
+        Vacante::create([
+            'titulo' => $datos['titulo'],
+            'salario_id' => $datos['salario'],
+            'categoria_id' => $datos['categoria'],
+            'empresa' => $datos['empresa'],
+            'ultimo_dia' => $datos['ultimo_dia'],
+            'descripcion' => $datos['descripcion'],
+            'imagen' => $datos['imagen'],
+            'user_id' => auth()->user()->id,
+        ]);
+
+        // Crear un mensaje
+        session()->flash('mensaje', 'La Vacante se publicó correctamente');
+
+        // Redireccionar al usuario
+        return redirect()->route('vacantes.index');
     }
 
     public function render()
